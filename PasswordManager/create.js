6b36dba6db68d9175af22bdbd133f6317f4c399f
rythem253 <miolavoro9@gmail.com>
2026-04-26 23:07:24 -0600

    password storage added not secure tho

--- PasswordManager/create.js
+++ PasswordManager/create.js
@@ -1,11 +1,7 @@
-/**
- * This file is designated to create and store new accounts into the database.
- */
-const createdUsername = document.getElementById("createUsername").value;
-const createdPassword = document.getElementById("createPassword").value;
+document.addEventListener("DOMContentLoaded", function () {
 
+    document.getElementById("createAccountBtn").addEventListener("click", function () {
+        alert("Done");
+    });
 
-document.getElementById("createAccountBtn").addEventListener("click", function() {
-   alert(createdUsername);
-   alert(createdPassword);
 });
--- PasswordManager/main.js
+++ PasswordManager/main.js
@@ -2,24 +2,57 @@
  * This file is designated to have the login page GUI backend.
  */
 
+function createUsername_Password() {
+
+    const newUsername = document.getElementById("createUsername").value;
+    const newPassword = document.getElementById("createPassword").value;
+    return {newUsername, newPassword};
+}
+
 //This function gets the value of the entered username and password and return
 //the value
 function getUsername_Password() {
+
     const username = document.getElementById("usernameInput").value;
     const password = document.getElementById("passwordInput").value;
-
     return {username, password};
 }
 
+const createBtn = document.getElementById("createAccountBtn");
+
+if (createBtn) {
+    createBtn.addEventListener("click", function () {
+    const createData = createUsername_Password();
+    localStorage.setItem(createData.newUsername, createData.newPassword);
+    alert("Account created");
+    window.location = "login.html";
+    });
+}
+
+
 //Once most of GUI and front end will be done, this function will be split into many
 //The encryption will have to check and match new logins. Most likely use a hashtable or linked list
-document.getElementById("loginBtn").addEventListener("click", function () {
-    const data = getUsername_Password();
+const loginBtn = document.getElementById("loginBtn");
 
-    if (data.username === "" || data.password === "") {
-        alert("Please fill in both fields");
-    } else if (data.username === "master" && data.password === "123") {
-        window.location.href = "dashboard.html";
-    }
-});
+if (loginBtn) {
+
+    loginBtn.addEventListener("click", function() {
+
+    const data = getUsername_Password();
+    const storedPassword = localStorage.getItem(data.username);
+
+        if (data.username === "" || data.password === "") {
+            alert("Please fill in both fields");
+        } else if 
+            (storedPassword === null) {
+            alert("User not exist");
+        } else if 
+            (storedPassword === data.password) {
+            window.location = "dashboard.html";
+        } else {
+            alert("IDK");
+        }
+    }); 
+}
+    
 
