68b453c9189380a09c4fffe267503643b38602a2
rythem253 <miolavoro9@gmail.com>
2026-06-05 22:17:02 -0600

    wip
    wip

--- PasswordManager/database.js
+++ PasswordManager/database.js
@@ -2,12 +2,4 @@ const Database = require('better-sqlite3');
 
 const db = new Database('app.db');
 
-db.prepare(`
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        password TEXT
-    )
-`).run();
-
 module.exports = db;
