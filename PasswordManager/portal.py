b559f590e225fb9e3eb4bf289760a599728c1564
rythem253 <miolavoro9@gmail.com>
2026-07-04 19:34:40 -0600

    WIP
    WIP

--- PasswordManager/portal.py
+++ PasswordManager/portal.py
@@ -7,15 +7,24 @@ class Dashboard(customtkinter.CTk):
         self.geometry("320x240")
         self.configure(fg_color="black")
 
-        self.message1 = customtkinter.CTkLabel(self, text="Welcome Back User !", font=("Arial", 24), text_color="royalblue")
+        #Frame_a, the main portal screen
+        self.frame_a = customtkinter.CTkFrame(self, fg_color="black")
+        self.frame_a.place(relwidth=1, relheight=1)
+
+        self.message1 = customtkinter.CTkLabel(self.frame_a, text="Welcome Back User !", font=("Arial", 24), text_color="royalblue")
         self.message1.pack(side="top", pady=20)
 
-        self.message2 = customtkinter.CTkLabel(self, text="Please select from options below:")
+        self.message2 = customtkinter.CTkLabel(self.frame_a, text="Please select from options below:")
         self.message2.pack(pady=5)
 
-        self.btnView = customtkinter.CTkButton(self, text="View Passwords")
-        self.btnAdd = customtkinter.CTkButton(self, text="Add Password")
-
+        self.btnView = customtkinter.CTkButton(self.frame_a, text="View Passwords")
         self.btnView.pack(pady=5)
+
+        self.btnAdd = customtkinter.CTkButton(self.frame_a, text="Add Password")
         self.btnAdd.pack(pady=5)
 
+        #Frame_b, to view saved passwords
+        #Display database
+        
+
+#.tkraise()
